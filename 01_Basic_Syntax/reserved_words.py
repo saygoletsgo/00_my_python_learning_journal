718963f0aa5eeb21810c5271978f7d07481ae52b
tmt <641896092@qq.com>
2019-10-29 22:55:45 +0800

    test

---
+++ 01_Basic_Syntax/reserved_words.py
@@ -0,0 +1,3 @@
+# The following list shows the Python keywords.
+# These are reserved words and you cannot use them as constant or variable or any other identifier names.
+# All the Python keywords contain lowercase letters only.
